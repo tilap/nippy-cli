9cf5a6de2f0b182c2ffe642003bb00a6a6102e64
Julien Tilap LA VINH <jlavinh@gmail.com>
2016-06-10 09:57:00 +0200

    Add spinner for long install tasks

--- dist/scripts/install/index.js
+++ dist/scripts/install/index.js
@@ -16,9 +16,14 @@ var _chalk = require('chalk');
 
 var _chalk2 = _interopRequireDefault(_chalk);
 
+var _cliSpinner = require('cli-spinner');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var runScript = require('../../library/runScript')(_path2.default.resolve(__dirname, '../..', 'scripts'));
+
+var spinner = new _cliSpinner.Spinner('  %s processing... please wait');
+spinner.setSpinnerString('|/-\\');
 
 runScript('scaffolder/index.js').then(function () {
   return runScript('setup/index.js');
@@ -27,7 +32,9 @@ runScript('scaffolder/index.js').then(function () {
     console.info('');
     console.info(_chalk2.default.green.bold('❯ Installing node dependancies...'));
     console.info(_chalk2.default.grey('  Could take a few minutes depending on your connection... Coffee time ;)'));
+    spinner.start();
     _child_process2.default.exec('npm install', function (err, stdout, stderr) {
+      spinner.stop();
       return resolve();
     });
   });
@@ -35,12 +42,18 @@ runScript('scaffolder/index.js').then(function () {
   return new _promise2.default(function (resolve, reject) {
     console.info('');
     console.info(_chalk2.default.green.bold('❯ Building the app for first time...'));
+    spinner.start();
     _child_process2.default.execSync('npm run build');
+    spinner.stop();
+
+    spinner.start();
     _child_process2.default.exec('npm run build', function (err, stdout, stderr) {
+      spinner.stop();
       return resolve();
     });
   });
 }).then(function () {
+  console.info('');
   console.info(_chalk2.default.green('✓ The api is installed'));
   console.info(_chalk2.default.grey('  If you don\'t already have a running mongodb, you can start "npm run db", then "npm run watch" to start the app.'));
   console.log('');
--- src/scripts/install/index.js
+++ src/scripts/install/index.js
@@ -1,8 +1,12 @@
 import childProcess from 'child_process';
 import path from 'path';
 import chalk from 'chalk';
+import { Spinner } from 'cli-spinner';
 
 const runScript = require('../../library/runScript')(path.resolve(__dirname, '../..', 'scripts'));
+
+const spinner = new Spinner('  %s processing... please wait');
+spinner.setSpinnerString('|/-\\');
 
 runScript('scaffolder/index.js')
   .then(() => runScript('setup/index.js'))
@@ -10,15 +14,27 @@ runScript('scaffolder/index.js')
     console.info('');
     console.info(chalk.green.bold('❯ Installing node dependancies...'));
     console.info(chalk.grey('  Could take a few minutes depending on your connection... Coffee time ;)'));
-    childProcess.exec('npm install', (err, stdout, stderr) => resolve());
+    spinner.start();
+    childProcess.exec('npm install', (err, stdout, stderr) => {
+      spinner.stop();
+      return resolve()
+    });
   }))
   .then(() => new Promise((resolve, reject) => {
     console.info('');
     console.info(chalk.green.bold('❯ Building the app for first time...'));
+    spinner.start();
     childProcess.execSync('npm run build');
-    childProcess.exec('npm run build', (err, stdout, stderr) => resolve());
+    spinner.stop();
+
+    spinner.start();
+    childProcess.exec('npm run build', (err, stdout, stderr) => {
+      spinner.stop();
+      return resolve()
+    });
   }))
   .then(() => {
+    console.info('');
     console.info(chalk.green('✓ The api is installed'));
     console.info(chalk.grey('  If you don\'t already have a running mongodb, you can start "npm run db", then "npm run watch" to start the app.'));
     console.log('');
